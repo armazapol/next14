040fd5bc5fbee0a302b2c08c001c3821adff8efd
pol <pol.armaza.002@gmail.com>
2023-12-12 10:45:49 -0500

    end course

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: 'Login',
